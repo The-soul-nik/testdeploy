309e521880c0b4822dd079f53826ff5ea8af9e6a
john <buzmakoff2005@mail.ru>
2020-06-26 19:04:49 +0300

    deploy an

---
+++ past/local_settings.py
@@ -0,0 +1,25 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+SECRET_KEY = 'uh^bna4r=m__e35$^e8@0**t7843d12l)aa&&b3n!2wt__ao8e'
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["boiling-reaches-46539.herokuapp.com", "localhost", "127.0.0.1"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
